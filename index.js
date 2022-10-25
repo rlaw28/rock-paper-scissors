1cb43a93ab7af13c5ae8e00a50ba1ecdf6a8d85b
rlaw28 <rashaunlawson28@gmail.com>
2022-10-25 18:20:48 -0400

    Create round function and game function.

--- index.js
+++ index.js
@@ -2,5 +2,38 @@
 
 function getComputerChoice() {
   choice = compChoice[Math.floor(Math.random() * compChoice.length)]
-    console.log(choice);
+    return choice;
 }
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == "Rock" && computerSelection == "Scissors") {
+    return "You Win! Rock beats Scissors.";
+  }
+  else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+    return "You Lose, Rock beats Scissors.";
+  }
+  else if (playerSelection == "Paper" && computerSelection == "Rock") {
+    return "You Win! Paper beats Rock."
+  }
+  else if (playerSelection == "Rock" && computerSelection == "Paper") {
+    return "You Lose, Paper beats Rock."
+  }
+  else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+    return "You Win! Scissors beats Paper."
+  }
+  else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+    return "You Lose, Scissors beats Paper"
+  }
+  else {
+    return "It's a Tie!"
+    }
+}   
+
+const computerSelection = getComputerChoice();
+
+function game() {
+  for (let i = 0; i < 5; i++){
+    playRound()
+  }
+ }
+
