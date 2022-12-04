35eb01819883d42a2f0b8096d57ce125787d559b
rlaw28 <rashaunlawson28@gmail.com>
2022-12-04 05:12:53 -0500

    Refactor variables and add new dom selectors.

--- index.js
+++ index.js
@@ -1,68 +1,86 @@
-let compChoice = ["rock", "paper", "scissors"]
+const userChoiceDisplay = document.getElementById('user-choice')
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoice
+let result
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+  userChoice = e.target.id
+  userChoiceDisplay.innerHTML = userChoice
+  getComputerChoice()
+  playRound()
+}));
+
 
 function getComputerChoice() {
-  choice = compChoice[Math.floor(Math.random() * compChoice.length)]
-    return choice;
+const compChoice = ["rock", "paper", "scissors"]
+const computerChoice = compChoice[Math.floor(Math.random() * compChoice.length)]
+  
+computerChoiceDisplay.innerHTML = computerChoice
 };
 
 
-  
-const playRound = (playerSelection, computerSelection) => {
-  if (playerSelection === computerSelection ){
-    return "It's a Tie!";
-    }
-  else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return "You Win! Rock beats Scissors.";
+function playRound() {
+  if (computerChoice === userChoice) {
+    result = 'Its a draw.'
   }
-  else if (playerSelection === "scissors" && computerSelection === "rock") {
-    return "You Lose, Rock beats Scissors.";
+  if (computerChoice === 'rock' && userChoice === 'paper') {
+    result = 'You Win!'
   }
-  else if (playerSelection === "paper" && computerSelection === "rock") {
-    return "You Win! Paper beats Rock.";
+  if (computerChoice === 'rock' && userChoice === 'scissors') {
+    result = 'You Lost!'
   }
-  else if (playerSelection === "rock" && computerSelection === "paper") {
-    return "You Lose, Paper beats Rock.";
+  if (computerChoice === 'paper' && userChoice === 'scissors') {
+    result = 'You Win!'
   }
-  else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return "You Win! Scissors beats Paper.";
+  if (computerChoice === 'paper' && userChoice === 'rock') {
+    result = 'You Lost!'
   }
-  else if (playerSelection === "paper" && computerSelection === "scissors") {
-    return "You Lose, Scissors beats Paper";
+  if (computerChoice === 'scissors' && userChoice === 'rock') {
+    result = 'You Win!'
   }
-    
-};  
+  if (computerChoice === 'scissors' && userChoice === 'paper') {
+    result = 'You Lost!'
+  }
+  resultDisplay.innerHTML = result
+}
+//   if (userChoice === computerChoice) {
+//     result = "Its a draw."
+//   }
+//  if (userChoice === "rock" && computerChoice === "scissors") {
+//    result = "You Win! Rock beats Scissors."
+//   }
+//   if (userChoice === "scissors" && computerChoice === "rock") {
+//     result = "You Lose, Rock beats Scissors."
+//   }
+//   if (userChoice === "paper" && computerChoice === "rock") {
+//     result = "You Win! Paper beats Rock."
+//   }
+//    if (userChoice === "rock" && computerChoice === "paper") {
+//    result = "You Lose, Paper beats Rock."
+//   }
+//    if (userChoice === "scissors" && computerChoice === "paper") {
+//     result = "You Win! Scissors beats Paper."
+//   }
+//    if (userChoice === "paper" && computerChoice === "scissors") {
+//     result = "You Lose, Scissors beats Paper"
+//    }
+ 
 
 
 
-const rock = document.querySelector('#rock');
-  
-rock.addEventListener('click', () => {
-  playerSelection = 'rock';
-  computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection))
-});
-
-  
-const paper = document.querySelector('#paper');
-  
-    paper.addEventListener('click', () => {
-      playerSelection = 'paper';
-      computerSelection = getComputerChoice();
-      console.log(playRound(playerSelection, computerSelection));
-});
 
 
-const scissors = document.querySelector('#scissors');
-  
-  scissors.addEventListener('click', () => {
-    playerSelection = 'scissors';
-    computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-});
 
 
 
   
-const results = document.querySelector('.results');
-results.innerText += result;
+
+
+
+
+
+
  
