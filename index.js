046c035418427834becb9efb5dc3cb81ef287edc
rlaw28 <rashaunlawson28@gmail.com>
2022-12-07 05:42:01 -0500

    Refactor gamer fucntion and fix variable scope.

--- index.js
+++ index.js
@@ -1,73 +1,48 @@
-const userChoiceDisplay = document.getElementById('user-choice')
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
-let userChoice
-let computerChoice
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const userPointsDisplay = document.getElementById('user-points');
+const computerPointsDisplay = document.getElementById('computer-points');
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+let player;
+let computer;
 let result
+let userPoints = 0;
+let computerPoints = 0;
 
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-  userChoice = e.target.id
-  userChoiceDisplay.innerHTML = userChoice
-  getComputerChoice()
-  playRound()
+
+choiceBtns.forEach(button => button.addEventListener('click', () => {
+  player = button.textContent
+  getComputerChoice();
+  playerText.innerHTML = `Player: ${player}`;
+  computerText.textContent = `Computer: ${computer}`;
+  resultText.textContent = gameResult();
 }));
 
 
 function getComputerChoice() {
-const compChoice = ["rock", "paper", "scissors"]
-const computerChoice = compChoice[Math.floor(Math.random() * compChoice.length)]
-  
-computerChoiceDisplay.innerHTML = computerChoice
+  let computerArray = ["rock", "paper", "scissors"];
+  computer = computerArray[Math.floor(Math.random() * computerArray.length)];
+};
+
+function gameResult() {
+  if (player == computer) {
+    return "Tie";
+  }
+  else if (computer == "rock") {
+    return (player == "paper") ? "You Win!" : "You Lose!"
+  }
+  else if (computer == "paper") {
+    return (player == "scissors") ? "You Win!" : "You Lose!"
+  }
+  else if (computer == "scissors") {
+    return (player == "rock") ? "You Win!" : "You Lose!"
+  }
 };
 
 
-function playRound() {
-  if (computerChoice === userChoice) {
-    result = 'Its a draw.'
-  }
-  if (computerChoice === 'rock' && userChoice === 'paper') {
-    result = 'You Win!'
-  }
-  if (computerChoice === 'rock' && userChoice === 'scissors') {
-    result = 'You Lost!'
-  }
-  if (computerChoice === 'paper' && userChoice === 'scissors') {
-    result = 'You Win!'
-  }
-  if (computerChoice === 'paper' && userChoice === 'rock') {
-    result = 'You Lost!'
-  }
-  if (computerChoice === 'scissors' && userChoice === 'rock') {
-    result = 'You Win!'
-  }
-  if (computerChoice === 'scissors' && userChoice === 'paper') {
-    result = 'You Lost!'
-  }
-  resultDisplay.innerHTML = result
-}
-//   if (userChoice === computerChoice) {
-//     result = "Its a draw."
-//   }
-//  if (userChoice === "rock" && computerChoice === "scissors") {
-//    result = "You Win! Rock beats Scissors."
-//   }
-//   if (userChoice === "scissors" && computerChoice === "rock") {
-//     result = "You Lose, Rock beats Scissors."
-//   }
-//   if (userChoice === "paper" && computerChoice === "rock") {
-//     result = "You Win! Paper beats Rock."
-//   }
-//    if (userChoice === "rock" && computerChoice === "paper") {
-//    result = "You Lose, Paper beats Rock."
-//   }
-//    if (userChoice === "scissors" && computerChoice === "paper") {
-//     result = "You Win! Scissors beats Paper."
-//   }
-//    if (userChoice === "paper" && computerChoice === "scissors") {
-//     result = "You Lose, Scissors beats Paper"
-//    }
- 
+
+
 
 
 
